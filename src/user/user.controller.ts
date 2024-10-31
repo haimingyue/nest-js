f5306e12e3712fcf9ee3f43d45e1db966efba7f6
Simoon <420526391@qq.com>
2024-10-31 21:01:03 +0800

    日志模块安装

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, Logger } from '@nestjs/common';
 import { UserService } from './user.service';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 
 @Controller('user')
 export class UserController {
 
+    // private logger = new Logger('AppModule')
+
 
     // 等于 this.userService = UserService
-    constructor(private userService: UserService) {
-
+    constructor(private userService: UserService, @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger) {
+        // this.logger.log("hello1")
+        this.logger.error("hello2")
+        this.logger.warn("hello3")
+        this.logger.verbose("hello4")
+        this.logger.debug("hello5")
     }
     
     @Get('/get')
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,66 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
+import { utilities as nestWinstonModuleUtilities, WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
+import 'winston-daily-rotate-file';
+
+const isDebug = process.env.NODE_ENV === 'development'
+
+function createDailyRotateTransport(level: string, filename: string) {
+  return new winston.transports.DailyRotateFile({
+    level,
+    dirname: 'logs',
+    datePattern: 'YYYY-MM-DD-HH',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.simple()
+    )
+  })
+}
 @Module({
 
-  imports: [PrismaModule],  // 导入 PrismaModule
+  imports: [PrismaModule, WinstonModule.forRoot({
+    level: 'silly',
+    transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('MyApp', {
+              colors: true,
+              prettyPrint: true,
+              processId: true,
+              appName: true,
+            }),
+          ),
+        }),
+      ...(isDebug ? [] : [
+        createDailyRotateTransport('warn', 'error'),
+        createDailyRotateTransport('info', 'app')
+      ])
+      
+      // other transports...
+      // new winston.transports.File({
+      //   filename: 'error.log',
+      //   level: 'error',
+      //   format: winston.format.combine(
+      //     winston.format.timestamp(),
+      //     winston.format.ms(),
+      //     nestWinstonModuleUtilities.format.nestLike('MyApp', {
+      //       colors: true,
+      //       prettyPrint: true,
+      //       processId: true,
+      //       appName: true,
+      //     }),
+      //   ),
+      // })
+      ],
+  })],  // 导入 PrismaModule
   controllers: [UserController],
   providers: [UserService]
 })
